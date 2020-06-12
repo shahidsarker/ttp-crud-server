711475be2c093d5b1b7c766cb5b8a292dfc3b3dd
Shahid <shahid.sarker@gmail.com>
2020-06-12 11:43:48 -0400

    Implement basic student model with campus association and firstName

---
+++ database/models/student.js
@@ -0,0 +1,8 @@
+const Sequelize = require("sequelize");
+const db = require("../db");
+
+const Student = db.define("student", {
+  firstName: { type: Sequelize.STRING, allowNull: false },
+});
+
+module.exports = Student;
