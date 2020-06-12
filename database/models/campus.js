3c6ef8b17a30e598e48bb8191b63c7341e33f9df
Shahid <shahid.sarker@gmail.com>
2020-06-12 11:30:42 -0400

    Update Campus model to match MVP requirements and update campus seeds

--- database/models/campus.js
+++ database/models/campus.js
@@ -3,9 +3,12 @@ const db = require("../db");
 
 const Campus = db.define("campus", {
   name: { type: Sequelize.STRING, allowNull: false },
-  address: { type: Sequelize.STRING, allowNull: true },
-  imageUrl: { type: Sequelize.STRING, allowNull: true },
-  description: { type: Sequelize.STRING, allowNull: true },
+  address: { type: Sequelize.STRING, allowNull: false },
+  imageUrl: {
+    type: Sequelize.STRING,
+    defaultValue: "https://via.placeholder.com/480x240?text=Placeholder",
+  },
+  description: { type: Sequelize.TEXT, defaultValue: "", allowNull: false },
 });
 
 module.exports = Campus;
