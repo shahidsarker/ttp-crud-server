a2e4cec748cd0d0aba0a401eaf23faf870b2186d
Marlon-Urena <45586481+Marlon-Urena@users.noreply.github.com>
2020-06-13 12:22:47 +0100

    Populated Student model with attributes and set some validation.

--- database/models/student.js
+++ database/models/student.js
@@ -1,9 +1,24 @@
 const Sequelize = require("sequelize");
-const { Campus } = require("./campus");
 const db = require("../db");
 
 const Student = db.define("student", {
   firstName: { type: Sequelize.STRING, allowNull: false },
+  lastName: { type: Sequelize.STRING, allowNull: false },
+  email: { type: Sequelize.STRING, allowNull: true, unique: true },
+  gpa: {
+    type: Sequelize.DECIMAL(2, 1),
+    allowNull: false,
+    defaultValue: 0.0,
+    validate: {
+      min: 0.0,
+      max: 4.0,
+    },
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    defaultValue: "https://via.placeholder.com/480x240?text=Placeholder",
+    allowNull: true,
+  },
 });
 
 module.exports = Student;
--- utils/seedDatabase.js
+++ utils/seedDatabase.js
@@ -23,45 +23,84 @@ const seedDatabase = async () => {
       address: "New York",
       imageUrl: "https://via.placeholder.com/150",
       description:
-          "orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        "orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
     }),
     Campus.create({
       name: "College1",
       address: "Lorensum",
       imageUrl: "https://via.placeholder.com/150",
       description:
-          "orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        "orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
     }),
     Campus.create({
       name: "College2",
       address: "Lorensum",
       imageUrl: "https://via.placeholder.com/150",
       description:
-          "orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        "orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
     }),
     Campus.create({
       name: "College3",
       address: "Lorensum",
       imageUrl: "https://via.placeholder.com/150",
       description:
-          "orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        "orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
     }),
     Campus.create({
       name: "College4",
       address: "Lorensum",
       imageUrl: "https://via.placeholder.com/150",
       description:
-          "orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        "orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
     }),
     Campus.create({
       name: "College5",
       address: "Lorensum",
       imageUrl: "https://via.placeholder.com/150",
       description:
-          "orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        "orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
     }),
-    Student.create({ firstName: "Daniel" }),
-    Student.create({ firstName: "Sally" }),
+    Student.create({
+      firstName: "Daniel",
+      lastName: "Angello",
+      email: "daniel@example.com",
+      gpa: "4.0",
+      campusId_FK: 1,
+    }),
+    Student.create({
+      firstName: "Sally",
+      lastName: "Bubbles",
+      email: "sally@example.com",
+      gpa: "0.1",
+      campusId_FK: 1,
+    }),
+    Student.create({
+      firstName: "George",
+      lastName: "Masterson",
+      email: "george@example.com",
+      campusId_FK: 1,
+    }),
+    Student.create({
+      firstName: "Robin",
+      lastName: "Sparkles",
+      email: "robin@example.com",
+      gpa: "3.5",
+      campusId_FK: 2,
+    }),
+    Student.create({
+      firstName: "Winnie",
+      lastName: "Dapoo",
+      email: "winnie@example.com",
+      gpa: "2.3",
+      campusId_FK: 2,
+    }),
+    Student.create({
+      firstName: "Tiger",
+      lastName: "Jump",
+      email: "tiger@example.com",
+      gpa: "3.0",
+      campusId_FK: 3,
+    }),
   ]);
 };
 
